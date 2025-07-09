1439e00b1810ef246439971ecd5f66469c1a6198
Diegomri <diegomri0102@gmail.com>
2025-07-08 21:48:19 -0400

    prueba

--- Backend/Meikan/Meikan/obj/Debug/net8.0/Meikan.AssemblyInfo.cs
+++ Backend/Meikan/Meikan/obj/Debug/net8.0/Meikan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Meikan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9fee47668627feeecd7e06883b4155c9616a2a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Meikan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Meikan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
